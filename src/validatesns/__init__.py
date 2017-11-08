8f4399eca49728e910437dc3a9963a46ed52b537
Marcin Maślany <m.maslany@clearcode.cc>
2017-11-08 14:27:13 +0100

    use proper signing cert url name.

--- src/validatesns/__init__.py
+++ src/validatesns/__init__.py
@@ -54,7 +54,7 @@ def validate(
     """
 
     # Check the signing certicate URL.
-    SigningCertURLValidator(certificate_url_regex).validate(message)
+    SigningCertUrlValidator(certificate_url_regex).validate(message)
 
     # Check the message age.
     if not isinstance(max_age, datetime.timedelta):
@@ -63,14 +63,14 @@ def validate(
 
     # Passed the basic checks, let's download the cert.
     # We've validated the URL, so aren't worried about a malicious server.
-    certificate = get_certificate(message["SigningCertURL"])
+    certificate = get_certificate(message["SigningCertUrl"])
 
     # Check the cryptographic signature.
     SignatureValidator(certificate).validate(message)
 
-class SigningCertURLValidator(object):
+class SigningCertUrlValidator(object):
     """
-    Validate a message's SigningCertURL is in the expected format.
+    Validate a message's SigningCertUrl is in the expected format.
     """
 
     def __init__(self, regex=DEFAULT_CERTIFICATE_URL_REGEX):
@@ -80,12 +80,12 @@ class SigningCertURLValidator(object):
         if not isinstance(message, dict):
             raise ValidationError("Unexpected message type {!r}".format(type(message).__name__))
 
-        url = message.get("SigningCertURL")
+        url = message.get("SigningCertUrl")
 
         if isinstance(url, six.string_types) and re.search(self.regex, url):
             return
 
-        raise ValidationError("SigningCertURL {!r} doesn't match required format {!r}".format(url, self.regex))
+        raise ValidationError("SigningCertUrl {!r} doesn't match required format {!r}".format(url, self.regex))
 
 class MessageAgeValidator(object):
     """
